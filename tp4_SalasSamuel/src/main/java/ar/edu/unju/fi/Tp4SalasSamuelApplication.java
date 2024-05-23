76d058c65fcedfb5fcdec2d1e9b235a7f8f9eb36
samuel-cell <salassamuel098@gmail.com>
2024-05-23 14:46:05 -0300

    Commit de inicio

---
+++ tp4_SalasSamuel/src/main/java/ar/edu/unju/fi/Tp4SalasSamuelApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4SalasSamuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4SalasSamuelApplication.class, args);
+	}
+
+}
